7f2b0387e2da759b169ea9b823fa25c45ae05eaa
Pallavi4546 <82462352+Pallavi4546@users.noreply.github.com>
2023-01-26 23:33:59 -0800

    added items to shop

--- src/Pages/HomePage.vue
+++ src/Pages/HomePage.vue
@@ -1,5 +1,25 @@
 <template>
-    <div class="container">
-        hi
-    </div>
+       <!-- <h1>Welcome to Designer Studio!</h1> -->
+       <div class="row my-1 container">
+        <div class="col-md-6 col-sm-12 ">
+            <img class="img-fluid m-auto" src="../assets/one.png" />
+        </div>
+        <div class="col-md-6">
+
+        </div>
+       </div>
 </template>
+<style scoped>
+*{
+    overflow: hidden;
+}
+h1{
+    font-family: Georgia, 'Times New Roman', Times, serif;
+    color: #EF7C8E;
+    text-align: center;
+}
+.img-fluid {
+    object-fit: cover;
+    width: 500px;
+}
+</style>
